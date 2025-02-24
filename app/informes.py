2ff0f0110f434830690ab8e6923d06422ad515f6
Fabrizzio <fabrizizocespedes@gmail.com>
2025-02-24 01:11:17 -0300

    Cambios en la logica del endpoint informes a posibles errores. Integracion al dash.

--- app/informes.py
+++ app/informes.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, send_file, Blueprint, redirect, render_template, url_for, flash, Response, make_response
-from config.config import get_db
+from .config.config import get_db
 from weasyprint import HTML
 
 bp = Blueprint('informes', __name__, url_prefix='/informes')
@@ -51,29 +51,37 @@ def exportar_pdf_libros():
 
 @bp.route('/exportar_pdf_prestamos', methods=['GET', 'POST'])
 def exportar_pdf_prestamos():
+    informes_pre = []  # Inicializar la variable antes de usarla
     if request.method == 'POST':
         filtro = request.form.get('filtro')
 
         db = get_db()
         with db.cursor() as cursor:
-            cursor.execute('''SELECT a.id, a.libro_id,d.titulo, a.fecha_prestamo, a.fecha_devolucion, a.estado,
-	b.full_name as Nom_Usuario,
-    b.username as Nick_Usuario,
-    CONCAT(c.nombres, ' ', c.apellidos) as Nom_Cliente    
-  FROM prestamos a
-  join usuarios b on a.usuario_id = b.id
-  join clientes c on a.cliente_id = c.id
-  join libros d on a.libro_id = d.id WHERE a.estado=%s''', (filtro,))
-            informes = cursor.fetchall()
+            cursor.execute('''SELECT a.id, a.libro_id, d.titulo, a.fecha_prestamo, a.fecha_devolucion, a.estado,
+                    b.full_name as Nom_Usuario,
+                    b.username as Nick_Usuario,
+                    CONCAT(c.nombres, ' ', c.apellidos) as Nom_Cliente    
+                FROM prestamos a
+                JOIN usuarios b ON a.usuario_id = b.id
+                JOIN clientes c ON a.cliente_id = c.id
+                JOIN libros d ON a.libro_id = d.id WHERE a.estado=%s''', (filtro,))
+            informes_pre = cursor.fetchall()  # Asignar el resultado a la variable
+            
+        # Verificar si se encontraron informes
+        if not informes_pre:
+            flash("No se encontraron informes para los criterios dados.", "warning")
+            return redirect(url_for('informes.formulario_pre'))
 
-         # Renderizar la plantilla HTML con los datos
-    html = render_template('informes/mostrar_informe_pre.html', informes=informes)
+        # Renderizar la plantilla HTML con los datos
+        html = render_template('informes/mostrar_informe_pre.html', informes_pre=informes_pre)
 
-    # Convertir HTML a PDF
-    pdf = HTML(string=html).write_pdf()
+        # Convertir HTML a PDF
+        pdf = HTML(string=html).write_pdf()
 
-    # Devolver el PDF como respuesta para mostrarlo en el navegador
-    response = make_response(pdf)
-    response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Content-Disposition'] = 'inline; filename=reporte.pdf'  # "inline" para mostrar en el navegador
-    return response
+        # Devolver el PDF como respuesta para mostrarlo en el navegador
+        response = make_response(pdf)
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-Disposition'] = 'inline; filename=reporte.pdf'  # "inline" para mostrar en el navegador
+        return response
+
+    return render_template('informes/realizar_informe_pre.html')
